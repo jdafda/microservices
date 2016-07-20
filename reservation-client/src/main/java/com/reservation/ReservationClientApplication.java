5f62b52e59721b46aa9200145ab6f032a8f9f1e5
Jitendra Dafda <jitudafda@gmail.com>
2016-07-19 23:27:07 -0400

    Modify reservation client

--- reservation-client/src/main/java/com/reservation/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/reservation/ReservationClientApplication.java
@@ -1,16 +1,80 @@
 package com.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservationClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationClientApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 }
+
+@RestController
+@RequestMapping("/reservations")
+class ReservationApiGatewayRestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/names", method = RequestMethod.GET)
+    public Collection<String> getReservationNames() {
+
+        discoveryClient.getServices().forEach(System.out::print);
+
+        ParameterizedTypeReference<Resources<Reservation>> parameterizedTypeReference = new ParameterizedTypeReference<Resources<Reservation>>() {
+        };
+
+        ResponseEntity<Resources<Reservation>> entity =
+                this.restTemplate.exchange("http://reservation-service/reservations", HttpMethod.GET, null, parameterizedTypeReference);
+
+        return entity
+                .getBody()
+                .getContent()
+                .stream()
+                .map(Reservation::getReservationName)
+                .collect(Collectors.toList());
+    }
+}
+
+class Reservation {
+    private String reservationName;
+
+    public String getReservationName() {
+        return reservationName;
+    }
+}
+
+
